dced1b522d6d95daa0aaa902ad0ceac00d6a643b
Ogb-Omar <157443920+Ogb-Omar@users.noreply.github.com>
2025-10-11 20:58:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
